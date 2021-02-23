36071e94f037859c073f4cb82e74176696343fc6
amiel.nachshon <amiel.nachshon@IL-AMIELN>
2021-02-24 01:11:44 +0200

    a

---
+++ Home_Exercise/leetcode/Reverse integer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_Exercise.leetcode
+{
+    class Revers_integer
+    {
+        public int reverseInteger (int number)
+        {
+            int revesreNum = 0;
+
+            while (number != 0)
+            {
+                revesreNum = revesreNum * 10 + number % 10;
+                number= number / 10;
+
+
+
+            }
+
+            return revesreNum;
+
+        }
+
+
+    }
+}
